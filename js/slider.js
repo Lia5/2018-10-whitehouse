217d9144c838a1be72c723da37b2aba5e4fec089
Елена Портная <e.portnaya@largespace.ru>
2018-11-13 12:22:12 +0200

    page 404

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,18 @@
-var slider = Peppermint(document.querySelector('#peppermint'), {
-        slideshow: true,
-        speed: 500,
-        touchSpeed: 300,
-        slideshowInterval: 3500,
-        dots: false
-    }),
+if (document.querySelector('#peppermint') != null) {
 
-    navLeft = document.querySelector('#peppermint-left'),
-    navRight = document.querySelector('#peppermint-right');
 
-navRight.addEventListener('click', slider.next, false);
-navLeft.addEventListener('click', slider.prev, false);
+    var slider = Peppermint(document.querySelector('#peppermint'), {
+            slideshow: true,
+            speed: 500,
+            touchSpeed: 300,
+            slideshowInterval: 3500,
+            dots: false
+        }),
+
+        navLeft = document.querySelector('#peppermint-left'),
+        navRight = document.querySelector('#peppermint-right');
+
+    navRight.addEventListener('click', slider.next, false);
+    navLeft.addEventListener('click', slider.prev, false);
+
+}
